619ee0675ffcd35d863b7d737f82fed33e1a5442
Karacky <mrnk3078@gmail.com>
2021-03-24 02:16:19 +0900

    refactor: test dto

--- test/infrastructure/mysql/room/dto_test.go
+++ test/infrastructure/mysql/room/dto_test.go
@@ -1,6 +1,7 @@
 package room
 
 import (
+	"errors"
 	"testing"
 
 	"github.com/oklog/ulid"
@@ -8,6 +9,7 @@ import (
 
 	domainModel "github.com/karamaru-alpha/chat-go-server/domain/model/room"
 	infra "github.com/karamaru-alpha/chat-go-server/infrastructure/mysql/room"
+	mockUtil "github.com/karamaru-alpha/chat-go-server/mock/util"
 	testdata "github.com/karamaru-alpha/chat-go-server/test/testdata"
 )
 
@@ -18,7 +20,10 @@ func TestToDTO(t *testing.T) {
 	// DTOに入れるEntityの準備
 	roomTitle, err := domainModel.NewTitle(testdata.Room.Title.Valid)
 	assert.NoError(t, err)
-	room, err := domainModel.Create(roomTitle)
+
+	factory := domainModel.NewFactory(mockUtil.GenerateULID)
+
+	room, err := factory.Create(roomTitle)
 	assert.NoError(t, err)
 
 	tests := []struct {
@@ -50,57 +55,47 @@ func TestToEntity(t *testing.T) {
 	dto := &infra.Room{ID: testdata.Room.ID.ValidPlain, Title: testdata.Room.Title.Valid}
 
 	// DTOから生成されるであろうEntityの準備
-	roomID, err := domainModel.NewID(&testdata.Room.ID.Valid)
-	assert.NoError(t, err)
 	roomTitle, err := domainModel.NewTitle(testdata.Room.Title.Valid)
 	assert.NoError(t, err)
-	room, err := domainModel.NewRoom(roomID, roomTitle)
+
+	factory := domainModel.NewFactory(mockUtil.GenerateULID)
+
+	room, err := factory.Create(roomTitle)
 	assert.NoError(t, err)
 
 	tests := []struct {
-		title    string
-		input    *infra.Room
-		expected *domainModel.Room
-		isError  bool
+		title     string
+		input     *infra.Room
+		expected1 *domainModel.Room
+		expected2 error
 	}{
 		{
-			title:    "【正常系】",
-			input:    dto,
-			expected: room,
-			isError:  false,
+			title:     "【正常系】",
+			input:     dto,
+			expected1: room,
+			expected2: nil,
 		},
 		{
-			title:    "【異常系】IDが不正値",
-			input:    &infra.Room{ID: testdata.Room.ID.InvalidPlain, Title: testdata.Room.Title.Valid},
-			expected: nil,
-			isError:  true,
+			title:     "【異常系】タイトルが不正値(short)",
+			input:     &infra.Room{ID: testdata.Room.ID.ValidPlain, Title: testdata.Room.Title.TooShort},
+			expected1: nil,
+			expected2: errors.New("RoomTitle should be Three to twenty characters"),
 		},
 		{
-			title:    "【異常系】タイトルが不正値(short)",
-			input:    &infra.Room{ID: testdata.Room.ID.ValidPlain, Title: testdata.Room.Title.TooShort},
-			expected: nil,
-			isError:  true,
-		},
-		{
-			title:    "【異常系】タイトルが不正値(long)",
-			input:    &infra.Room{ID: testdata.Room.ID.ValidPlain, Title: testdata.Room.Title.TooLong},
-			expected: nil,
-			isError:  true,
+			title:     "【異常系】タイトルが不正値(long)",
+			input:     &infra.Room{ID: testdata.Room.ID.ValidPlain, Title: testdata.Room.Title.TooLong},
+			expected1: nil,
+			expected2: errors.New("RoomTitle should be Three to twenty characters"),
 		},
 	}
 
 	for _, td := range tests {
 		td := td
 
-		output, err := infra.ToEntity(td.input)
+		output1, output2 := infra.ToEntity(td.input)
 
-		assert.Equal(t, td.expected, output)
-
-		if td.isError {
-			assert.Error(t, err)
-		} else {
-			assert.NoError(t, err)
-		}
+		assert.Equal(t, td.expected1, output1)
+		assert.Equal(t, td.expected2, output2)
 	}
 }
 
@@ -112,64 +107,52 @@ func TestToEntities(t *testing.T) {
 	dto := infra.Room{ID: testdata.Room.ID.ValidPlain, Title: testdata.Room.Title.Valid}
 
 	// DTOから生成されるであろうEntityの準備
-	roomID, err := domainModel.NewID(&testdata.Room.ID.Valid)
-	assert.NoError(t, err)
-
 	roomTitle, err := domainModel.NewTitle(testdata.Room.Title.Valid)
 	assert.NoError(t, err)
 
-	room, err := domainModel.NewRoom(roomID, roomTitle)
+	factory := domainModel.NewFactory(mockUtil.GenerateULID)
+
+	room, err := factory.Create(roomTitle)
 	assert.NoError(t, err)
 
 	tests := []struct {
-		title    string
-		input    *[]infra.Room
-		expected *[]domainModel.Room
-		isError  bool
+		title     string
+		input     *[]infra.Room
+		expected1 *[]domainModel.Room
+		expected2 error
 	}{
 		{
-			title:    "【正常系】1つのDTOをEntityに変換",
-			input:    &[]infra.Room{dto},
-			expected: &[]domainModel.Room{*room},
-			isError:  false,
+			title:     "【正常系】1つのDTOをEntityに変換",
+			input:     &[]infra.Room{dto},
+			expected1: &[]domainModel.Room{*room},
+			expected2: nil,
 		},
 		{
-			title:    "【正常系】2つのDTOをEntityに変換",
-			input:    &[]infra.Room{dto, dto},
-			expected: &[]domainModel.Room{*room, *room},
-			isError:  false,
+			title:     "【正常系】2つのDTOをEntityに変換",
+			input:     &[]infra.Room{dto, dto},
+			expected1: &[]domainModel.Room{*room, *room},
+			expected2: nil,
 		},
 		{
-			title:    "【異常系】IDが不正値",
-			input:    &[]infra.Room{{ID: testdata.Room.ID.InvalidPlain, Title: testdata.Room.Title.Valid}},
-			expected: nil,
-			isError:  true,
+			title:     "【異常系】タイトルが不正値(short)",
+			input:     &[]infra.Room{{ID: testdata.Room.ID.ValidPlain, Title: testdata.Room.Title.TooShort}},
+			expected1: nil,
+			expected2: errors.New("RoomTitle should be Three to twenty characters"),
 		},
 		{
-			title:    "【異常系】タイトルが不正値(short)",
-			input:    &[]infra.Room{{ID: testdata.Room.ID.ValidPlain, Title: testdata.Room.Title.TooShort}},
-			expected: nil,
-			isError:  true,
-		},
-		{
-			title:    "【異常系】タイトルが不正値(long)",
-			input:    &[]infra.Room{{ID: testdata.Room.ID.ValidPlain, Title: testdata.Room.Title.TooLong}},
-			expected: nil,
-			isError:  true,
+			title:     "【異常系】タイトルが不正値(long)",
+			input:     &[]infra.Room{{ID: testdata.Room.ID.ValidPlain, Title: testdata.Room.Title.TooLong}},
+			expected1: nil,
+			expected2: errors.New("RoomTitle should be Three to twenty characters"),
 		},
 	}
 
 	for _, td := range tests {
 		td := td
 
-		output, err := infra.ToEntities(td.input)
+		output1, output2 := infra.ToEntities(td.input)
 
-		assert.Equal(t, td.expected, output)
-
-		if td.isError {
-			assert.Error(t, err)
-		} else {
-			assert.NoError(t, err)
-		}
+		assert.Equal(t, td.expected1, output1)
+		assert.Equal(t, td.expected2, output2)
 	}
 }
