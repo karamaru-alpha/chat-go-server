b44e5b8bb7ae71cf2f4b23a1ab9893d751bc941f
Karacky <mrnk3078@gmail.com>
2021-03-30 23:28:52 +0900

    refactor: ulidGeneratorをコンストラクタで生成する

--- util/ulid_generator.go
+++ util/ulid_generator.go
@@ -7,8 +7,20 @@ import (
 	"github.com/oklog/ulid"
 )
 
-// GenerateULID ランダムなULIDを生成する処理
-func GenerateULID() ulid.ULID {
+// IULIDGenerator ULID生成のインターフェース
+type IULIDGenerator interface {
+	Generate() ulid.ULID
+}
+
+type ulidGenerator struct{}
+
+// NewULIDGenerator ULID生成処理のコンストラクタ
+func NewULIDGenerator() IULIDGenerator {
+	return &ulidGenerator{}
+}
+
+// Generate ランダムなULIDを生成する処理
+func (ulidGenerator) Generate() ulid.ULID {
 	t := time.Now()
 	entropy := ulid.Monotonic(rand.New(rand.NewSource(t.UnixNano())), 0)
 	return ulid.MustNew(ulid.Timestamp(t), entropy)
