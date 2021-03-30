81d8c4faca98b703386c3b11b80cc7c4a1f3aada
Karacky <mrnk3078@gmail.com>
2021-03-31 01:30:34 +0900

    feat: dto convert entity to rGCP

---
+++ interfaces/controller/room/dto.go
@@ -0,0 +1,25 @@
+package room
+
+import (
+	"github.com/oklog/ulid"
+
+	domainModel "github.com/karamaru-alpha/chat-go-server/domain/model/room"
+	pb "github.com/karamaru-alpha/chat-go-server/interfaces/proto/pb"
+)
+
+// ToProto トークルームエンティティをｇRPCの型に変換
+func ToProto(entity *domainModel.Room) *pb.Room {
+	return &pb.Room{
+		Id:    ulid.ULID(entity.ID).String(),
+		Title: string(entity.Title),
+	}
+}
+
+// ToProtos トークルームエンティティのスライスをｇRPCの型に変換
+func ToProtos(entities *[]domainModel.Room) []*pb.Room {
+	var rooms []*pb.Room
+	for _, v := range *entities {
+		rooms = append(rooms, ToProto(&v))
+	}
+	return rooms
+}
