0429eaa9b1d41329a8f0b9b9b4daa545376bc9e1
Karacky <mrnk3078@gmail.com>
2021-04-07 20:26:26 +0900

    feat: メッセージ作成のアプリケーションサービス

---
+++ application/message/create/interactor.go
@@ -0,0 +1,61 @@
+package message
+
+import (
+	"github.com/oklog/ulid"
+
+	messageDomain "github.com/karamaru-alpha/chat-go-server/domain/model/message"
+	roomDomain "github.com/karamaru-alpha/chat-go-server/domain/model/room"
+)
+
+type interactor struct {
+	factory           messageDomain.IFactory
+	messageRepository messageDomain.IRepository
+	roomRepository    roomDomain.IRepository
+}
+
+// NewInteractor メッセージ作成アプリケーションサービスのコンストラクタ
+func NewInteractor(
+	f messageDomain.IFactory,
+	mr messageDomain.IRepository,
+	rr roomDomain.IRepository,
+) IInputPort {
+	return &interactor{
+		factory:           f,
+		messageRepository: mr,
+		roomRepository:    rr,
+	}
+}
+
+// Handle メッセージ作成アプリケーションサービス
+func (i interactor) Handle(input InputData) OutputData {
+
+	parsedULID, err := ulid.Parse(input.RoomID)
+	if err != nil {
+		return OutputData{Err: err}
+	}
+	roomID, err := roomDomain.NewID(&parsedULID)
+	if err != nil {
+		return OutputData{Err: err}
+	}
+
+	room, err := i.roomRepository.Find(roomID)
+	if err != nil {
+		return OutputData{Err: err}
+	}
+
+	messageBody, err := messageDomain.NewBody(input.Body)
+	if err != nil {
+		return OutputData{Err: err}
+	}
+
+	message, err := i.factory.Create(room, messageBody)
+	if err != nil {
+		return OutputData{Err: err}
+	}
+
+	if err = i.messageRepository.Create(message); err != nil {
+		return OutputData{Err: err}
+	}
+
+	return OutputData{Message: message}
+}
---
+++ application/message/create/output_data.go
@@ -0,0 +1,11 @@
+package message
+
+import (
+	domain "github.com/karamaru-alpha/chat-go-server/domain/model/message"
+)
+
+// OutputData メッセージ作成のアプリケーションサービスの出力
+type OutputData struct {
+	Message *domain.Message
+	Err     error
+}
