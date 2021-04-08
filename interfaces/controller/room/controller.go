f5bf75080dc9e675dc2f66091fc339514452ed6c
Karacky <mrnk3078@gmail.com>
2021-04-09 04:38:31 +0900

    feat: roomJoinController

--- interfaces/controller/room/controller.go
+++ interfaces/controller/room/controller.go
@@ -5,43 +5,60 @@ import (
 
 	createApplication "github.com/karamaru-alpha/chat-go-server/application/room/create"
 	findAllApplication "github.com/karamaru-alpha/chat-go-server/application/room/find_all"
+	joinApplication "github.com/karamaru-alpha/chat-go-server/application/room/join"
+	messageDTO "github.com/karamaru-alpha/chat-go-server/interfaces/dto/message"
+	roomDTO "github.com/karamaru-alpha/chat-go-server/interfaces/dto/room"
 	pb "github.com/karamaru-alpha/chat-go-server/proto/pb"
 )
 
 type controller struct {
 	createApplication  createApplication.IInputPort
 	findAllApplication findAllApplication.IInputPort
+	joinApplication    joinApplication.IInputPort
 }
 
 // NewController gRPC経由のリクエストを捌くControllerのコンストラクタ
-func NewController(c createApplication.IInputPort, f findAllApplication.IInputPort) pb.RoomServicesServer {
+func NewController(
+	c createApplication.IInputPort,
+	f findAllApplication.IInputPort,
+	j joinApplication.IInputPort,
+) pb.RoomServicesServer {
 	return &controller{
 		createApplication:  c,
 		findAllApplication: f,
+		joinApplication:    j,
 	}
 }
 
 // CreateRoom トークルームを作成するController
-func (h controller) CreateRoom(ctx context.Context, request *pb.CreateRoomRequest) (*pb.CreateRoomResponse, error) {
-
+func (c controller) CreateRoom(ctx context.Context, request *pb.CreateRoomRequest) (*pb.CreateRoomResponse, error) {
 	input := createApplication.InputData{Title: request.Title}
-	output := h.createApplication.Handle(input)
 
+	output := c.createApplication.Handle(input)
 	if output.Err != nil {
 		return nil, output.Err
 	}
 
-	return &pb.CreateRoomResponse{Room: ToProto(output.Room)}, nil
+	return &pb.CreateRoomResponse{Room: roomDTO.ToProto(output.Room)}, nil
 }
 
 // GetRooms トークルーム全件取得のController
-func (h controller) GetRooms(ctx context.Context, _ *pb.GetRoomsRequest) (*pb.GetRoomsResponse, error) {
-
-	output := h.findAllApplication.Handle()
-
+func (c controller) GetRooms(ctx context.Context, _ *pb.GetRoomsRequest) (*pb.GetRoomsResponse, error) {
+	output := c.findAllApplication.Handle()
 	if output.Err != nil {
 		return nil, output.Err
 	}
 
-	return &pb.GetRoomsResponse{Rooms: ToProtos(output.Rooms)}, nil
+	return &pb.GetRoomsResponse{Rooms: roomDTO.ToProtos(output.Rooms)}, nil
+}
+
+func (c controller) JoinRoom(ctx context.Context, request *pb.JoinRoomRequest) (*pb.JoinRoomResponse, error) {
+	input := joinApplication.InputData{RoomID: request.RoomId}
+
+	output := c.joinApplication.Handle(input)
+	if output.Err != nil {
+		return nil, output.Err
+	}
+
+	return &pb.JoinRoomResponse{Messages: messageDTO.ToProtos(output.Messages)}, nil
 }
--- test/interfaces/controller/room/contoller_test.go
+++ test/interfaces/controller/room/contoller_test.go
@@ -2,6 +2,7 @@ package room_test
 
 import (
 	"context"
+	"errors"
 	"testing"
 
 	"github.com/golang/mock/gomock"
@@ -9,12 +10,18 @@ import (
 
 	createApplication "github.com/karamaru-alpha/chat-go-server/application/room/create"
 	findAllApplication "github.com/karamaru-alpha/chat-go-server/application/room/find_all"
-	domain "github.com/karamaru-alpha/chat-go-server/domain/model/room"
+	joinApplication "github.com/karamaru-alpha/chat-go-server/application/room/join"
+	messageDomain "github.com/karamaru-alpha/chat-go-server/domain/model/message"
+	roomDomain "github.com/karamaru-alpha/chat-go-server/domain/model/room"
 	controller "github.com/karamaru-alpha/chat-go-server/interfaces/controller/room"
+	messageDTO "github.com/karamaru-alpha/chat-go-server/interfaces/dto/message"
+	roomDTO "github.com/karamaru-alpha/chat-go-server/interfaces/dto/room"
 	mockCreateApplication "github.com/karamaru-alpha/chat-go-server/mock/application/room/create"
 	mockFindAllApplication "github.com/karamaru-alpha/chat-go-server/mock/application/room/find_all"
+	mockJoinApplication "github.com/karamaru-alpha/chat-go-server/mock/application/room/join"
 	pb "github.com/karamaru-alpha/chat-go-server/proto/pb"
-	tdDomain "github.com/karamaru-alpha/chat-go-server/test/testdata/domain/room"
+	tdMessageDomain "github.com/karamaru-alpha/chat-go-server/test/testdata/domain/message"
+	tdRoomDomain "github.com/karamaru-alpha/chat-go-server/test/testdata/domain/room"
 	tdString "github.com/karamaru-alpha/chat-go-server/test/testdata/string"
 )
 
@@ -29,8 +36,9 @@ func TestGetRooms(t *testing.T) {
 	// applicationをモック
 	createUsecase := mockCreateApplication.NewMockIInputPort(ctrl)
 	findAllUsecase := mockFindAllApplication.NewMockIInputPort(ctrl)
+	joinUsecase := mockJoinApplication.NewMockIInputPort(ctrl)
 
-	handler := controller.NewController(createUsecase, findAllUsecase)
+	handler := controller.NewController(createUsecase, findAllUsecase, joinUsecase)
 
 	tests := []struct {
 		title     string
@@ -44,12 +52,12 @@ func TestGetRooms(t *testing.T) {
 			title: "【正常系】トークルームが1つ",
 			before: func() {
 				findAllUsecase.EXPECT().Handle().Return(findAllApplication.OutputData{
-					Rooms: &[]domain.Room{tdDomain.Room.Entity}, Err: nil,
+					Rooms: &[]roomDomain.Room{tdRoomDomain.Room.Entity}, Err: nil,
 				})
 			},
 			input1:    context.TODO(),
 			input2:    &pb.GetRoomsRequest{},
-			expected1: &pb.GetRoomsResponse{Rooms: controller.ToProtos(&[]domain.Room{tdDomain.Room.Entity})},
+			expected1: &pb.GetRoomsResponse{Rooms: roomDTO.ToProtos(&[]roomDomain.Room{tdRoomDomain.Room.Entity})},
 			expected2: nil,
 		},
 		{
@@ -91,25 +99,54 @@ func TestCreateRoom(t *testing.T) {
 	// applicationをモック
 	createUsecase := mockCreateApplication.NewMockIInputPort(ctrl)
 	findAllUsecase := mockFindAllApplication.NewMockIInputPort(ctrl)
-	createUsecase.EXPECT().Handle(
-		createApplication.InputData{Title: tdString.Room.Title.Valid},
-	).Return(
-		createApplication.OutputData{Room: &tdDomain.Room.Entity, Err: nil},
-	)
-
-	handler := controller.NewController(createUsecase, findAllUsecase)
+	joinUsecase := mockJoinApplication.NewMockIInputPort(ctrl)
+	handler := controller.NewController(createUsecase, findAllUsecase, joinUsecase)
 
 	tests := []struct {
 		title     string
+		before    func()
 		input     *pb.CreateRoomRequest
 		expected1 *pb.CreateRoomResponse
 		expected2 error
 	}{
 		{
-			title:     "【正常系】",
+			title: "【正常系】トークルーム作成",
+			before: func() {
+				createUsecase.EXPECT().Handle(
+					createApplication.InputData{Title: tdString.Room.Title.Valid},
+				).Return(
+					createApplication.OutputData{Room: &tdRoomDomain.Room.Entity, Err: nil},
+				)
+			},
 			input:     &pb.CreateRoomRequest{Title: tdString.Room.Title.Valid},
-			expected1: &pb.CreateRoomResponse{Room: controller.ToProto(&tdDomain.Room.Entity)},
+			expected1: &pb.CreateRoomResponse{Room: roomDTO.ToProto(&tdRoomDomain.Room.Entity)},
 			expected2: nil,
+		},
+		{
+			title: "【異常系】タイトルが不正値(empty)",
+			before: func() {
+				createUsecase.EXPECT().Handle(
+					createApplication.InputData{Title: ""},
+				).Return(
+					createApplication.OutputData{Room: nil, Err: errors.New("error")},
+				)
+			},
+			input:     &pb.CreateRoomRequest{Title: ""},
+			expected1: nil,
+			expected2: errors.New("error"),
+		},
+		{
+			title: "【異常系】タイトルが不正値(long)",
+			before: func() {
+				createUsecase.EXPECT().Handle(
+					createApplication.InputData{Title: tdString.Room.Title.TooLong},
+				).Return(
+					createApplication.OutputData{Room: nil, Err: errors.New("error")},
+				)
+			},
+			input:     &pb.CreateRoomRequest{Title: tdString.Room.Title.TooLong},
+			expected1: nil,
+			expected2: errors.New("error"),
 		},
 	}
 
@@ -117,7 +154,89 @@ func TestCreateRoom(t *testing.T) {
 		td := td
 
 		t.Run("CreateRoom:"+td.title, func(t *testing.T) {
+			td.before()
+
 			output1, output2 := handler.CreateRoom(context.TODO(), td.input)
+
+			assert.Equal(t, td.expected1, output1)
+			assert.Equal(t, td.expected2, output2)
+		})
+	}
+}
+
+// TestJoinRoom トークルーム入室Controllerのテスト
+func TestJoinRoom(t *testing.T) {
+	t.Parallel()
+
+	// go-mockの開始
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+
+	// applicationをモック
+	createUsecase := mockCreateApplication.NewMockIInputPort(ctrl)
+	findAllUsecase := mockFindAllApplication.NewMockIInputPort(ctrl)
+	joinUsecase := mockJoinApplication.NewMockIInputPort(ctrl)
+	handler := controller.NewController(createUsecase, findAllUsecase, joinUsecase)
+
+	tests := []struct {
+		title     string
+		before    func()
+		input     *pb.JoinRoomRequest
+		expected1 *pb.JoinRoomResponse
+		expected2 error
+	}{
+		{
+			title: "【正常系】トークルーム入室",
+			before: func() {
+				joinUsecase.EXPECT().Handle(
+					joinApplication.InputData{RoomID: tdString.Room.ID.Valid},
+				).Return(
+					joinApplication.OutputData{
+						Messages: &[]messageDomain.Message{tdMessageDomain.Message.Entity}, Err: nil,
+					},
+				)
+			},
+			input: &pb.JoinRoomRequest{RoomId: tdString.Room.ID.Valid},
+			expected1: &pb.JoinRoomResponse{
+				Messages: messageDTO.ToProtos(&[]messageDomain.Message{tdMessageDomain.Message.Entity}),
+			},
+			expected2: nil,
+		},
+		{
+			title: "【異常系】RoomIDが不正値",
+			before: func() {
+				joinUsecase.EXPECT().Handle(
+					joinApplication.InputData{RoomID: tdString.Room.ID.Invalid},
+				).Return(
+					joinApplication.OutputData{Messages: nil, Err: errors.New("error")},
+				)
+			},
+			input:     &pb.JoinRoomRequest{RoomId: tdString.Room.ID.Invalid},
+			expected1: nil,
+			expected2: errors.New("error"),
+		},
+		{
+			title: "【異常系】RoomIDが空",
+			before: func() {
+				joinUsecase.EXPECT().Handle(
+					joinApplication.InputData{RoomID: ""},
+				).Return(
+					joinApplication.OutputData{Messages: nil, Err: errors.New("error")},
+				)
+			},
+			input:     &pb.JoinRoomRequest{RoomId: ""},
+			expected1: nil,
+			expected2: errors.New("error"),
+		},
+	}
+
+	for _, td := range tests {
+		td := td
+
+		t.Run("JoinRoom:"+td.title, func(t *testing.T) {
+			td.before()
+
+			output1, output2 := handler.JoinRoom(context.TODO(), td.input)
 
 			assert.Equal(t, td.expected1, output1)
 			assert.Equal(t, td.expected2, output2)
