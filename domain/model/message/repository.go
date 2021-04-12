e1c60f961b3700770c044d5accd77fd918ab0436
Karacky <mrnk3078@gmail.com>
2021-04-12 21:35:50 +0900

    faet: domain層実装

--- domain/model/message/repository.go
+++ domain/model/message/repository.go
@@ -1,9 +1,14 @@
 package message
 
-import "github.com/karamaru-alpha/chat-go-server/domain/model/room"
+import (
+	"context"
+
+	"github.com/karamaru-alpha/chat-go-server/domain/model/room"
+)
 
 // IRepository メッセージエンティティの永続化・再構築を実現するリポジトリ
 type IRepository interface {
-	Save(*Message) error
+	Save(context.Context, *Message) error
 	FindAll(*room.ID) (*[]Message, error)
+	Subscribe(context.Context, *room.ID, chan Message) error
 }
