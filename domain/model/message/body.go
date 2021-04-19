1ea293d16035e67cb8dc147ff0e28f15021d07f7
Karacky <mrnk3078@gmail.com>
2021-04-20 02:12:59 +0900

    refactor: マジックナンバーをなくす

--- domain/model/message/body.go
+++ domain/model/message/body.go
@@ -2,8 +2,12 @@ package message
 
 import (
 	"errors"
+	"fmt"
 	"unicode/utf8"
 )
+
+const BODY_MIN_LEN = 1
+const BODY_MAX_LEN = 255
 
 // Body メッセージ本文を表す値オブジェクト
 type Body string
@@ -13,8 +17,8 @@ func NewBody(v string) (Body, error) {
 	if v == "" {
 		return "", errors.New("MessageBody is empty")
 	}
-	if utf8.RuneCountInString(v) == 0 || utf8.RuneCountInString(v) > 255 {
-		return "", errors.New("MessageBody should be 1 to 255 characters")
+	if utf8.RuneCountInString(v) < BODY_MIN_LEN || utf8.RuneCountInString(v) > BODY_MAX_LEN {
+		return "", fmt.Errorf("MessageBody should be %d to %d characters", BODY_MIN_LEN, BODY_MAX_LEN)
 	}
 
 	return Body(v), nil
